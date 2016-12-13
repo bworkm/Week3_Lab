3dd445a323c1139b35288a4cab6e1149259319cf
Brian Workman <bworkm@outlook.com>
2016-12-12 21:31:11 -0800

    image randomizer working with duplication verification

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 'use strict';
 
 var startingArrayList = [
-  ['bag', '.jpg'],
-  ['banana', '.jpg'],
-  ['bathroom', '.jpg'],
-  ['boots', '.jpg'],
-  ['breakfast', '.jpg'],
-  ['bubblegum', '.jpg'],
-  ['chair', '.jpg'],
-  ['cthulhu', '.jpg'],
-  ['dog-duck', '.jpg'],
-  ['dragon', '.jpg'],
-  ['pen', '.jpg'],
-  ['pet-sweep', '.jpg'],
-  ['scissors', '.jpg'],
-  ['shark', '.jpg'],
-  ['sweep', '.png'],
-  ['tauntaun', '.jpg'],
-  ['unicorn', '.jpg'],
-  ['usb', '.gif'],
-  ['water-can', '.jpg'],
-  ['wine-glass', '.jpg']
+  ['bag', '.jpg'],        // 0
+  ['banana', '.jpg'],     // 1
+  ['bathroom', '.jpg'],   // 2
+  ['boots', '.jpg'],      // 3
+  ['breakfast', '.jpg'],  // 4
+  ['bubblegum', '.jpg'],  // 5
+  ['chair', '.jpg'],      // 6
+  ['cthulhu', '.jpg'],    // 7
+  ['dog-duck', '.jpg'],   // 8
+  ['dragon', '.jpg'],     // 9
+  ['pen', '.jpg'],        // 10
+  ['pet-sweep', '.jpg'],  // 11
+  ['scissors', '.jpg'],   // 12
+  ['shark', '.jpg'],      // 13
+  ['sweep', '.png'],      // 14
+  ['tauntaun', '.jpg'],   // 15
+  ['unicorn', '.jpg'],    // 16
+  ['usb', '.gif'],        // 17
+  ['water-can', '.jpg'],  // 18
+  ['wine-glass', '.jpg']  // 19
 ];
 var elSurvey = document.getElementById('survey');
 var elBegin = document.getElementById('begin');
@@ -39,7 +39,6 @@ function SurveyItem(name, ext) {
 //*********************************
 function displayImageChoices() {
   var tempVar = toBeDisplayed[0].filepath;
-  console.log(toBeDisplayed);
   document.getElementById('image1').src = tempVar;
   tempVar = toBeDisplayed[1].filepath;
   document.getElementById('image2').src = tempVar;
@@ -50,12 +49,14 @@ function displayImageChoices() {
 //*********************************
 function getRandomImage() {  //Generates random number to select an item from surveyItemList array.
   for (var i = 0; i < 3; i++) {
-    console.log(surveyItemList.length,'Before moveChoice');
+    // console.log(surveyItemList.length,'Before moveChoice');
     var tempRand = Math.floor(Math.random() * surveyItemList.length);
     // console.log(tempRand,'Random number');
+    // console.log(surveyItemList[tempRand]);
     moveChoice(tempRand);
-    console.log(surveyItemList.length,'After moveChoice');
+    // console.log(surveyItemList.length,'After moveChoice');
   }
+  console.log(toBeDisplayed.length,'toBeDisplayed length before returnChoice');
   returnChoice();
   displayImageChoices();
 }
@@ -63,21 +64,27 @@ function getRandomImage() {  //Generates random number to select an item from su
 //Use the unShift() array method to place the new item at the beginning of the toBeDisplayed array.
 //Moves currently displayed images from surveyItemList array to toBeDisplayed array.
 function moveChoice(position) {
+  // console.log(toBeDisplayed,'toBeDisplayed begin moveChoice');
   toBeDisplayed.unshift(surveyItemList[position]);
   surveyItemList.splice(position, 1);
+  // console.log(toBeDisplayed,'toBeDisplayed end moveChoice');
 }
 //*********************************
 //Returns previous choices back to surveyItemList array.
 //Use the pop() method to remove the last item from toBeDisplayed. Use push() to add it back to surveyItemList.
 function returnChoice() {
   if (toBeDisplayed.length > 3) {
-    console.log('entered returnChoice')
-    for (var j = toBeDisplayed.length; j > 3; j--) {
+    console.log('entered returnChoice');
+    // console.log(toBeDisplayed.length,'toBeDisplayed length (before loop)');
+    for (var j = toBeDisplayed.length - 1; j >= 3; j--) {
+      console.log(j,'value of J');
       surveyItemList.push(toBeDisplayed[j]);
       toBeDisplayed.pop();
-      console.log(surveyItemList.length,'After returnChoice');
+      console.log(toBeDisplayed.length,'toBeDisplayed length (after pop)');
+      // console.log(toBeDisplayed,'toBeDisplayed in returnChoice loop');
     }
   }
+  console.log(toBeDisplayed,'toBeDisplayed outside returnChoice');
 }
 //*********************************
 for (var i = 0; i < startingArrayList.length; i++) {
@@ -89,9 +96,12 @@ for (var i = 0; i < startingArrayList.length; i++) {
 }
 //*********************************
 function handleClickBegin() {
-  displayImageChoices(toBeDisplayed);
-  elBegin.display = none;     //This is not working yet. Needs to hide one button and display the other.
-  elResults.display = block;
+  console.log(surveyItemList.length);
+  console.log(toBeDisplayed.length,'toBeDisplayed length beginning');
+  getRandomImage();
+  // displayImageChoices(toBeDisplayed);
+  // elBegin.display = none;     //This is not working yet. Needs to hide one button and display the other.
+  // elResults.display = block;
 }
 //*********************************
 
